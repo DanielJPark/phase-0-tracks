b2df1661c943a8d5cfe89be727b74ec729e6df88
DanielJPark <danpark80@gmail.com>
2016-04-18 10:32:43 -0500

    5.1

--- Ruby/method_detective.rb
+++ Ruby/method_detective.rb
@@ -5,51 +5,56 @@
 # committed the crime, add additional calls to prove it.
 
 puts "iNvEsTiGaTiOn".swapcase
- # => “InVeStIgAtIoN”
+# => "InVeStIgAtIoN"
 
-puts "zom".sub("zom", "zoom")
-puts "zom".gsub("o", "oo")
- # => “zoom”
+puts "zom".sub("zom","zoom")
+puts "zom".gsub("o","oo")
+puts "zom".sub("o", "oo")
+puts "zom".delete("m").concat("om")
+puts "zom".chop + ("om")
+puts "zom".chars.insert(1,"o").join
+# => "zoom"
 
 puts "enhance".center(15)
- # => "    enhance    "
+puts "enhance".sub("enhance", "    enhance    ")
+puts "enhance".rjust(11, "    ") + ("    ")
+puts ("enhance".reverse + ("    ")).reverse + ("    ")
+#=> "    enhance    "
 
 puts "Stop! You’re under arrest!".upcase
- # => "STOP! YOU’RE UNDER ARREST!"
+puts "Stop! You're under arrest!".downcase!.swapcase
 
 puts "the usual".concat " suspects"
 puts "the usual" + " suspects"
 puts "the usual" << " suspects"
-puts "the usual".sub("usual", "usual suspects")
-puts "the usual".gsub("usual", "usual suspects")
+puts "the usual".sub("the usual", "the usual suspects")
+#=> "the usual suspects"
 
- #=> "the usual suspects"
+puts " suspects".rjust(18, "the usual")
+puts " suspects".sub(" ", "the usual ")
+#=> "the usual suspects"
 
-puts " suspects".rjust(12,"the usual")
-# # => "the usual suspects"
-
-puts "The case of the disappearing last letter".chomp("r")
 puts "The case of the disappearing last letter".chop
-# # => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chomp("r")
+puts "The case of the disappearing last letter".slice(0..38)
+# => "The case of the disappearing last lette"
 
-puts "The mystery of the missing first letter".slice(1..38)
 puts "The mystery of the missing first letter".delete("T")
+puts "The mystery of the missing first letter".slice(1..38)
+puts "The mystery of the missing first letter".reverse.chop.reverse
+# => "he mystery of the missing first letter"
 
-# # => "he mystery of the missing first letter"
-puts  "Elementary,    my   dear        Watson!".split.join(" ")
-puts  "Elementary,    my   dear        Watson!".squeeze
-puts  "Elementary,    my   dear        Watson!".
+puts "Elementary,    my   dear        Watson!".squeeze
+puts "Elementary,    my   dear        Waston!".split.join(" ")
+# => "Elementary, my dear Watson!"
 
-# # => "Elementary, my dear Watson!"
-
+puts "z".ord
 puts "z".codepoints
 # => 122 
-
 # (What is the significance of the number 122 in relation to the character z?)
-#122 is the codepoint of "z"
+#It is the codepoint of the character "z"
 
 puts "How many times does the letter 'a' appear in this string?".count("a")
-# # => 4
+#=> 4
 
-
-
+#p "hello".delete[2]
