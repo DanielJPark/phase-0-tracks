867b07212dfbd89184522f97502b79a4056e4e2c
DanielJPark <danpark80@gmail.com>
2016-05-07 10:21:52 -0500

    hear_request method added

--- Ruby/Ruby_Assessment/firstname_middleinitial_lastname/parent.rb
+++ Ruby/Ruby_Assessment/firstname_middleinitial_lastname/parent.rb
@@ -12,12 +12,29 @@ class Parent
 		puts "*yaaaawn* I'm up, I'm up. #{@name} is up. What's the matter?"
 	end
 
+	def hear_request(request)
+		if request.split(" ").length <= @hours_slept
+			@past_requests[request] = "Accepted"
+			puts "Okay, since you said #{request} so nicely."
+			p @past_requests
+			true
+		else
+			@past_requests[request] = "Rejected"
+			puts "Not today, sweetie."
+			p @past_requests
+			false
+		end
+	end
+
 end
 
 
 
 #driver code
 
-p bob = Parent.new("Bob", 6)
-p mary = Parent.new("Mary", 4)
-p bob.greet_child
+bob = Parent.new("Bob", 6)
+mary = Parent.new("Mary", 4)
+bob.greet_child
+
+p bob.hear_request("Can I get a toy today")
+p bob.hear_request("I would like to to go the movies today and shopping afterwards.")
