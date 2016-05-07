a055fe6607b25f136ec17f8d50d2d7b50968ec3c
DanielJPark <danpark80@gmail.com>
2016-05-07 10:09:16 -0500

    greet child method created

--- Ruby/Ruby_Assessment/firstname_middleinitial_lastname/parent.rb
+++ Ruby/Ruby_Assessment/firstname_middleinitial_lastname/parent.rb
@@ -1,8 +1,22 @@
 class Parent
 
+	attr_accessor :name
+
 	def initialize(name, hours_slept)
 		@name = name
 		@hours_slept = hours_slept
 	end
 
+	def greet_child
+		puts "*yaaaawn* I'm up, I'm up. #{@name} is up. What's the matter?"
+	end
+
 end
+
+
+
+#driver code
+
+p bob = Parent.new("Bob", 6)
+p mary = Parent.new("Mary", 4)
+p bob.greet_child
