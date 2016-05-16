0f9f82eca8cea355e5ef46d9fff62d716df6685a
DanielJPark <danpark80@gmail.com>
2016-05-16 00:40:30 -0500

    add applicant method

--- database/8.5/job_application.rb
+++ database/8.5/job_application.rb
@@ -11,7 +11,7 @@ create_applicants_table = <<-SQL
 	CREATE TABLE IF NOT EXISTS applicants (
 		id INTEGER PRIMARY KEY,
 		applicant_name VARCHAR(255),
-		age INT,
+		applicant_age INT,
 		position_desired VARCHAR(255)
 	)
 SQL
@@ -26,3 +26,18 @@ SQL
 db.execute(create_applicants_table)
 db.execute(create_positions_table)
 
+def user_interface
+end
+
+def add_applicant
+	puts "Please enter your full name:"
+	applicant_name = gets.chomp
+	puts "Please enter your age:"
+	applicant_age = gets.chomp
+	puts "Please enter the position you are interested in:"
+	position_desired = gets.chomp
+
+
+	db.execute("INSERT INTO applicants (applicant_name, applicant_age, position_desired) VALUES (?, ?, ?)" , [applicant_name, applicant_age, position_desired])
+	user_interface
+end
